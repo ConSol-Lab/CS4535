1e626bb3356666a9b934765415dd162a2b24950e
Imko Marijnissen <imko.marijnissen@gmail.com>
2026-02-20 11:29:23 +0100

    feat: add hamiltonian path test cases

--- implementation/tests/propagators/circuit_tests/circuit_checker_tests.rs
+++ implementation/tests/propagators/circuit_tests/circuit_checker_tests.rs
@@ -1,9 +1,18 @@
 #![cfg(test)]
 
+use std::rc::Rc;
+
+use drcp_format::IntAtomic;
+use implementation::propagators::circuit::CircuitChecker;
+use pumpkin_checking::InferenceChecker;
+use pumpkin_checking::VariableState;
+
 use crate::Propagator;
 use crate::propagators::ProofTestRunner;
 use crate::propagators::TSP_DIRECT_INSTANCES;
 use crate::propagators::TSP_INSTANCES;
+use crate::propagators::model::Atomic;
+use crate::propagators::model::Variable;
 
 #[test]
 fn circuit_checker_test_0() {
@@ -87,4 +96,42 @@ fn circuit_checker_test_3_invalid() {
     if let Err(e) = result {
         panic!("Failed to check inference: {e:#?}");
     }
+}
+use fzn_rs::VariableExpr;
+
+#[test]
+fn circuit_hamiltonian_path_checker() {
+    let x: Variable = VariableExpr::Identifier(Rc::from("x")).into();
+    let y: Variable = VariableExpr::Identifier(Rc::from("y")).into();
+    let z: Variable = VariableExpr::Identifier(Rc::from("z")).into();
+
+    let premises = vec![
+        Atomic::IntAtomic(IntAtomic {
+            name: "x".into(),
+            comparison: drcp_format::IntComparison::Equal,
+            value: 2,
+        }),
+        Atomic::IntAtomic(IntAtomic {
+            name: "y".into(),
+            comparison: drcp_format::IntComparison::Equal,
+            value: 3,
+        }),
+        Atomic::IntAtomic(IntAtomic {
+            name: "z".into(),
+            comparison: drcp_format::IntComparison::Equal,
+            value: 1,
+        }),
+    ];
+
+    let variable_state = VariableState::prepare_for_conflict_check(premises.clone(), None)
+        .expect("no mutually exclusive atomics");
+
+    let checker = CircuitChecker {
+        successors: vec![x, y, z],
+    };
+
+    assert!(
+        !checker.check(variable_state, &premises, None),
+        "If there is a cycle concerning all variables, then there is no conflict, so the checker should not pas it"
+    )
 }
--- implementation/tests/propagators/circuit_tests/circuit_propagation_tests.rs
+++ implementation/tests/propagators/circuit_tests/circuit_propagation_tests.rs
@@ -4,6 +4,10 @@
     reason = "Will be refactored in the future using the state API"
 )]
 
+use implementation::propagators::circuit::CircuitConstructor;
+use implementation::propagators::circuit::CircuitExplanationType;
+use pumpkin_core::state::State;
+
 use crate::Propagator;
 use crate::propagators::ProofTestRunner;
 use crate::propagators::TSP_DIRECT_INSTANCES;
@@ -50,4 +54,29 @@ fn circuit_propagation_test_3() {
     if let Err(e) = result {
         panic!("Failed to check inference: {e:#?}");
     }
+}
+
+#[test]
+fn circuit_hamiltonian_path_propagation() {
+    let mut state = State::default();
+
+    let x = state.new_interval_variable(2, 2, None);
+    let y = state.new_interval_variable(3, 3, None);
+    let z = state.new_interval_variable(1, 1, None);
+
+    let constraint_tag = state.new_constraint_tag();
+
+    let _ = state.add_propagator(CircuitConstructor {
+        successors: vec![x, y, z].into(),
+        constraint_tag,
+        conflict_detection_only: false,
+        explanation_type: CircuitExplanationType::Direct,
+    });
+
+    let result = state.propagate_to_fixed_point();
+
+    assert!(
+        result.is_ok(),
+        "If there is a cycle concerning all variables, then no conflict should be reported"
+    )
 }
