581b6354753864f053e3900f971676cc9df702b3
Imko Marijnissen <imko.marijnissen@gmail.com>
2026-02-24 11:22:34 +0100

    feat: post all-different decomposition when posting circuit

--- pumpkin-crates/constraints/src/constraints/circuit.rs
+++ pumpkin-crates/constraints/src/constraints/circuit.rs
@@ -4,6 +4,8 @@ use pumpkin_core::constraints::Constraint;
 use pumpkin_core::proof::ConstraintTag;
 use pumpkin_core::variables::IntegerVariable;
 
+use crate::constraints;
+
 /// Creates the [`Constraint`] that enforces that all the given `variables` are distinct.
 pub fn circuit<Var: IntegerVariable + 'static>(
     variables: impl Into<Box<[Var]>>,
@@ -12,11 +14,44 @@ pub fn circuit<Var: IntegerVariable + 'static>(
     explanation_type: CircuitExplanationType,
 ) -> impl Constraint {
     let variables: Box<[Var]> = variables.into();
-
-    CircuitConstructor {
-        successors: variables,
+    Circuit {
+        variables,
         constraint_tag,
         conflict_detection_only,
         explanation_type,
+    }
+}
+
+#[derive(Debug)]
+pub struct Circuit<Var> {
+    variables: Box<[Var]>,
+    constraint_tag: ConstraintTag,
+    conflict_detection_only: bool,
+    explanation_type: CircuitExplanationType,
+}
+
+impl<Var: IntegerVariable + 'static> Constraint for Circuit<Var> {
+    fn post(
+        self,
+        solver: &mut pumpkin_core::Solver,
+    ) -> Result<(), pumpkin_core::ConstraintOperationError> {
+        constraints::all_different(self.variables.clone(), self.constraint_tag, false, true)
+            .post(solver)?;
+
+        CircuitConstructor {
+            successors: self.variables,
+            constraint_tag: self.constraint_tag,
+            conflict_detection_only: self.conflict_detection_only,
+            explanation_type: self.explanation_type,
+        }
+        .post(solver)
+    }
+
+    fn implied_by(
+        self,
+        _solver: &mut pumpkin_core::Solver,
+        _reification_literal: pumpkin_core::variables::Literal,
+    ) -> Result<(), pumpkin_core::ConstraintOperationError> {
+        unimplemented!()
     }
 }
